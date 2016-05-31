24dd9933ae0a438307ef07d6253549c7953904fe
Chris Gaddes <cagaddes@gmail.com>
2016-05-31 14:09:45 -0500

    Here we go

--- app/src/main/java/com/nkdroid/vectordrawable/MainActivity.java
+++ app/src/main/java/com/nkdroid/vectordrawable/MainActivity.java
@@ -1,21 +1,68 @@
 package com.nkdroid.vectordrawable;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
     private ImageView img;
+
+
+    Button button; // Makes variable called button
+
+    // Triggers when view is clicked. v is the view that has been clicked
+
+    public void onClick(View v) {
+
+        Drawable dr = ContextCompat.getDrawable(this, R.drawable.button_pressed);
+        dr.setColorFilter(Color.parseColor("#FF0000"), PorterDuff.Mode.SRC_ATOP);
+
+        v.setBackgroundColor(Color.GRAY);
+
+        switch (v.getId()) {
+            case R.id.btn:
+
+                if (button == null) {
+                    button = (Button) findViewById(v.getId());
+                } else {
+                    button.setBackgroundResource(R.drawable.button_pressed);
+                    button = (Button) findViewById(v.getId());
+                }
+                button.setBackgroundDrawable(dr);
+
+
+            case R.id.btn2:
+                if (button == null) {
+                    button = (Button) findViewById(v.getId());
+                } else {
+                    button.setBackgroundResource(R.drawable.button_pressed);
+                    button = (Button) findViewById(v.getId());
+                }
+                button.setBackgroundDrawable(dr);
+
+                break;
+
+            default:
+                break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        img = (ImageView) findViewById(R.id.imgProgramatically);
-        img.setImageResource(R.drawable.vector_image);
+         }
 
-    }
+
 
 
 }
