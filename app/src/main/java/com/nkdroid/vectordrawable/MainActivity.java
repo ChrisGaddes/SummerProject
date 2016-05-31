482350ce5a9c7bb557bab90601933be19d1b60b1
Chris Gaddes <cagaddes@gmail.com>
2016-05-31 16:14:59 -0500

    Removed legacy code

--- app/src/main/java/com/nkdroid/vectordrawable/MainActivity.java
+++ app/src/main/java/com/nkdroid/vectordrawable/MainActivity.java
@@ -5,25 +5,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+/**
+ * Created by cagad on 5/31/2016.
+ */
+
 public class MainActivity extends Activity {
 
 
     private Button btnDemo;
-    private boolean isPink = true;
+    private boolean isClicked = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.button_demo_activity);
 
-        btnDemo = (Button) findViewById(R.id.btnDemo);
+        btnDemo = (Button) findViewById(R.id.btn_1);
+
         btnDemo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                isPink = !isPink;
-                int resId = isPink ? R.drawable.btn_unpressed : R.drawable.btn_pressed;
+                isClicked = !isClicked;
+                int resId = isClicked ? R.drawable.btn_unpressed : R.drawable.btn_pressed;
                 btnDemo.setBackgroundResource(resId);
-                btnDemo.setText(isPink ? "PINK" : "BLUE");
+                //btnDemo.setText(isClicked ? "PINK" : "BLUE");
             }
         });
     }
