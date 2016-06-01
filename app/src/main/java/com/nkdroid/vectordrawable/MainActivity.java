2d8350c0c46d351b7fa3e0e1834f0d93e8bc9105
Chris Gaddes <cagaddes@gmail.com>
2016-06-01 13:29:08 -0500

    Fixed bug where you had to double click on arrows to make them appear

--- app/src/main/java/com/nkdroid/vectordrawable/MainActivity.java
+++ app/src/main/java/com/nkdroid/vectordrawable/MainActivity.java
@@ -2,75 +2,80 @@ package com.nkdroid.vectordrawable;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private static final String TAG = "CAG"; //TODO Figure out what tag is
+    Toolbar mToolbar;
+    Button mBtnAdd;
+    boolean isClicked;
+
+    // initializes arrow buttons
+    Button arrow_west;
+    Button arrow_north;
+    Button arrow_east;
+    Button arrow_south;
+
+    // initializes arrow states to unclicked
+    boolean isBtnArrowEastClicked = false;
+    boolean isBtnArrowWestClicked = false;
+    boolean isBtnArrowNorthClicked = false;
+    boolean isBtnArrowSouthClicked = false;
 
 
-    private Button btn_arrow_east;
-    private Button btn_arrow_west;
-    private Button btn_arrow_north;
-    private Button btn_arrow_south;
-
-    private boolean isClicked = true;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.AppTheme);
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_arrow_east = (Button) findViewById(R.id.btn_1);
+        // find View-elements
+        arrow_east = (Button) findViewById(R.id.btn_1);
+        arrow_west = (Button) findViewById(R.id.btn_2);
+        arrow_north = (Button) findViewById(R.id.btn_3);
+        arrow_south = (Button) findViewById(R.id.btn_4);
 
-        btn_arrow_east.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                isClicked = !isClicked;
-                int resId = isClicked ? R.drawable.btn_unpressed : R.drawable.btn_pressed_east;
-                btn_arrow_east.setBackgroundResource(resId);
-                //btn_arrow_east.setText(isClicked ? "PINK" : "BLUE");
-            }
-        });
-
-        btn_arrow_west = (Button) findViewById(R.id.btn_2);
-
-        btn_arrow_west.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                isClicked = !isClicked;
-                int resId = isClicked ? R.drawable.btn_unpressed : R.drawable.btn_pressed_west;
-                btn_arrow_west.setBackgroundResource(resId);
-                //btn_arrow_east.setText(isClicked ? "PINK" : "BLUE");
-            }
-        });
+        // assign listeners to buttons
+        arrow_west.setOnClickListener(this);
+        arrow_north.setOnClickListener(this);
+        arrow_east.setOnClickListener(this);
+        arrow_south.setOnClickListener(this);
+    }
 
 
-        btn_arrow_north = (Button) findViewById(R.id.btn_3);
-
-        btn_arrow_north.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                isClicked = !isClicked;
-                int resId = isClicked ? R.drawable.btn_unpressed : R.drawable.btn_pressed_north;
-                btn_arrow_north.setBackgroundResource(resId);
-                //btn_arrow_east.setText(isClicked ? "PINK" : "BLUE");
-            }
-        });
-
-        btn_arrow_south = (Button) findViewById(R.id.btn_4);
-
-        btn_arrow_south.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                isClicked = !isClicked;
-                int resId = isClicked ? R.drawable.btn_unpressed : R.drawable.btn_pressed_south;
-                btn_arrow_south.setBackgroundResource(resId);
-                //btn_arrow_east.setText(isClicked ? "PINK" : "BLUE");
-            }
-        });
+    @Override
+    public void onClick(View v) {
+        // define the button that invoked the listener by id
 
 
-
+        switch (v.getId()) {
+            case R.id.btn_1:
+                //arrow_west.setText("OK button was clicked");
+                Toast.makeText(MainActivity.this, "btn1", Toast.LENGTH_SHORT).show();
+                isBtnArrowEastClicked = !isBtnArrowEastClicked; // cycles arrow and box
+                int resId1 = isBtnArrowEastClicked ? R.drawable.btn_unpressed : R.drawable.btn_pressed_east;
+                arrow_east.setBackgroundResource(resId1); // sets background
+                break;
+            case R.id.btn_2:
+                Toast.makeText(MainActivity.this, "btn2", Toast.LENGTH_SHORT).show();
+                isBtnArrowWestClicked = !isBtnArrowWestClicked; // cycles arrow and box
+                int resId2 = isBtnArrowWestClicked ? R.drawable.btn_unpressed : R.drawable.btn_pressed_west;
+                arrow_west.setBackgroundResource(resId2); // sets background
+                break;
+            case R.id.btn_3:
+                isBtnArrowNorthClicked = !isBtnArrowNorthClicked; // cycles arrow and box
+                int resId3 = isBtnArrowNorthClicked ? R.drawable.btn_unpressed : R.drawable.btn_pressed_north;
+                arrow_north.setBackgroundResource(resId3); // sets background
+                break;
+            case R.id.btn_4:
+                isBtnArrowSouthClicked = !isBtnArrowSouthClicked; // cycles arrow and box
+                int resId4 = isBtnArrowSouthClicked ? R.drawable.btn_unpressed : R.drawable.btn_pressed_south;
+                arrow_south.setBackgroundResource(resId4); // sets background
+                break;
+        }
     }
 }
