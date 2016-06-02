1c010f143ec5fb37bccb8eb4f3f347977953ef0c
Chris Gaddes <cagaddes@gmail.com>
2016-06-01 20:34:58 -0500

    Fixed errors.

--- app/src/main/java/com/chrisgaddes/freebodydiagram/MainActivity.java
+++ app/src/main/java/com/chrisgaddes/freebodydiagram/MainActivity.java
@@ -75,25 +75,25 @@ public class MainActivity extends Activity implements View.OnClickListener {
             case R.id.btn_1:
                 //arrow_west.setText("OK button was clicked");
                 Toast.makeText(MainActivity.this, "btn1", Toast.LENGTH_SHORT).show();
-                isBtnArrowEastClicked = !isBtnArrowEastClicked; // cycles arrow and box
-                int resId1 = isBtnArrowEastClicked ? R.drawable.btn_unpressed : R.drawable.btn_pressed_east;
+                                int resId1 = isBtnArrowEastClicked ? R.drawable.btn_unpressed : R.drawable.btn_pressed_east;
                 arrow_east.setBackgroundResource(resId1); // sets background
+                isBtnArrowEastClicked = !isBtnArrowEastClicked; // cycles arrow and box
                 break;
             case R.id.btn_2:
                 Toast.makeText(MainActivity.this, "btn2", Toast.LENGTH_SHORT).show();
-                isBtnArrowWestClicked = !isBtnArrowWestClicked; // cycles arrow and box
                 int resId2 = isBtnArrowWestClicked ? R.drawable.btn_unpressed : R.drawable.btn_pressed_west;
                 arrow_west.setBackgroundResource(resId2); // sets background
+                isBtnArrowWestClicked = !isBtnArrowWestClicked; // cycles arrow and box
                 break;
             case R.id.btn_3:
-                isBtnArrowNorthClicked = !isBtnArrowNorthClicked; // cycles arrow and box
                 int resId3 = isBtnArrowNorthClicked ? R.drawable.btn_unpressed : R.drawable.btn_pressed_north;
                 arrow_north.setBackgroundResource(resId3); // sets background
+                isBtnArrowNorthClicked = !isBtnArrowNorthClicked; // cycles arrow and box
                 break;
             case R.id.btn_4:
-                isBtnArrowSouthClicked = !isBtnArrowSouthClicked; // cycles arrow and box
                 int resId4 = isBtnArrowSouthClicked ? R.drawable.btn_unpressed : R.drawable.btn_pressed_south;
                 arrow_south.setBackgroundResource(resId4); // sets background
+                isBtnArrowSouthClicked = !isBtnArrowSouthClicked; // cycles arrow and box
                 break;
         }
     }
